1422c50c6eab068f9af0588ef2b66f3408541d3b
FernandoTarso <fernando.tarso.git@gmail.com>
2024-06-16 22:51:51 -0300

    add recursive fibonacci - not implemented

--- src/br/com/fernandotarso/Fibonacci.java
+++ src/br/com/fernandotarso/Fibonacci.java
@@ -4,19 +4,52 @@ public class Fibonacci {
 
    public static void fibonacciSequence(int terms) {
 
-      if (terms > 93) {
-         terms = 93;
+      terms = 100;
+
+      if (terms > 92) {
+         terms = 92;
       }
 
-      long x_1 = -1;
+      long x_1 = 0;
       long x_2 = 1;
-      long sum;
+      long sum = x_1;
 
-      for (int i = 0; i < terms ; i++) {
+      for (int i = 0; i < terms; i++) {
+         System.out.println(sum);
          sum = x_1 + x_2;
          x_1 = x_2;
          x_2 = sum;
-         System.out.println(sum);
+
       }
    }
 }
+//   public static void fibo() {
+//      fibo(0, 1, 0, 10);
+//   }
+//   private static void fibo(long a, long b, int counter, int terms) {
+//      long sum = a;
+//      System.out.println(sum);
+//      sum = a+b;
+//      counter++;
+//      if (counter<terms){
+//         fibo(b, sum, counter, terms);
+//      }
+//   }
+
+//   public static void fibonacciSequence(int terms) {
+//
+//      if (terms > 93) {
+//         terms = 93;
+//      }
+//
+//      long x_1 = -1;
+//      long x_2 = 1;
+//      long sum;
+//
+//      for (int i = 0; i < terms ; i++) {
+//         sum = x_1 + x_2;
+//         x_1 = x_2;
+//         x_2 = sum;
+//         System.out.println(sum);
+//      }
+//   }
